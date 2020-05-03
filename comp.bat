9baa5dd27de3a5f4101615227acba8d689678334
maciektr <mtratnowiecki@gmail.com>
2020-05-03 19:17:18 +0200

    Move to masm

--- comp.bat
+++
@@ -1,6 +0,0 @@
-set program=%1
-echo Compiling %program%.asm program with nasm
-NASM\NASM.EXE -f OBJ %program%.asm -o %program%.obj
-
-echo Linking %program%.obj program with val
-VAL\VAL.EXE %program%.obj
