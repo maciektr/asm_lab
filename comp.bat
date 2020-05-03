a41fc5003e16564086080328d8ac9c4cee53c36b
maciektr <mtratnowiecki@gmail.com>
2020-05-03 16:46:25 +0200

    Update helper script

--- comp.bat
+++ comp.bat
@@ -1,5 +1,6 @@
 set program=%1
-echo Compiling %program%.asm program 
-
+echo Compiling %program%.asm program with nasm
 NASM\NASM.EXE -f OBJ %program%.asm -o %program%.obj
+
+echo Linking %program%.obj program with val
 VAL\VAL.EXE %program%.obj
